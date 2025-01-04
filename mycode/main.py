b9f5be99b86ab86b032c2a35be897374c5df9d96
HHgzs <3166237657@qq.com>
2025-01-04 16:01:25 +0800

    初始化

---
+++ mycode/main.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+
+# Load a model
+# model = YOLO("yolo11n.yaml")
+# model = YOLO("yolo11n.pt")
+model = YOLO("ultralytics/cfg/models/11/yolo11.yaml").load("mycode/yolo11n.pt")
+
+# Train the model
+results = model.train(data="ultralytics/cfg/datasets/coco8.yaml", epochs=100, imgsz=640)
